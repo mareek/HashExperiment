9eb27e2371bb7c940b08980a32e84a216b44cef2
Matthieu Mourisson <mareek@gmail.com>
2024-10-06 19:25:44 +0200

    Add project files.

---
+++ HashExperiment.Test/QuickAndDirtyHashTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using NFluent;
+using Xunit;
+
+namespace HashExperiment.Test;
+
+public class QuickAndDirtyHashTest
+{
+    [Fact]
+    public void TestHash()
+    {
+        Span<byte[]> testSet =
+        [
+            [0],
+            [1],
+            [255],
+            [1,2,3,4],
+            RandomNumberGenerator.GetBytes(16),
+            RandomNumberGenerator.GetBytes(16),
+            RandomNumberGenerator.GetBytes(256),
+            RandomNumberGenerator.GetBytes(2048),
+        ];
+
+        List<byte[]> resultSet = [];
+
+        foreach (var testArray in testSet)
+        {
+            var firstHash = new QuickAndDirtyHash().Hash(testArray);
+            var secondHash = new QuickAndDirtyHash().Hash(testArray);
+            Check.That(firstHash).IsEqualTo(secondHash);
+            resultSet.Add(firstHash);
+        }
+
+        for (int i = 0; i < resultSet.Count; i++)
+        {
+            for (int j = i + 1; j < resultSet.Count; j++)
+            {
+                Check.That(resultSet[i]).IsNotEqualTo(resultSet[j]);
+            }
+        }
+    }
+}
